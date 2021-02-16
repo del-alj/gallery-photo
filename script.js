98d71cab77d23f5febd6167d30e4a2a16d01ef0f
dino <woo.dong.7.wd@gmail.com>
2021-02-16 16:46:46 +0100

    for test

--- script.js
+++ script.js
@@ -2,23 +2,42 @@ let data = ["1", "name image", "year"];
 
 function main()
 {
+  var data = {
+    name: ["the lake", "look", "tower", "throne"],
+    year: ["2020", "2020", "2020", "2020"],
+    path: ['./resources/pictures/pic']
+    };
+
     // <h1>d</h1>
-    let bigBox = document.getElementById("contener");
-    for (let i = 0; i < 10; i++)
-    {
-        let element = ft_creatChild("div", "Div", "group-divs", 1);
-        ft_style_Div(element);
-        bigBox.appendChild(element);
-        // ft_creatChaildsOfChaild(element, data);
-    }
-    scrollDivs();
+  let bigBox = document.getElementById("contener");
+  for (let i = 0; i < 4; i++) {
+    let element = ft_creatChild("div", "Div", "group-divs", i);
+    bigBox.appendChild(element);
+    /*add pic*/
+    let Div = document.getElementById("Div" + i);
+    let img = document.createElement("img");
+    img.className = "group-imgs";
+    img.src = ft_name_pic(data.path, i + 1); 
+    // img.style.height = "100px"
+    Div.appendChild(img);
+    /*add div comment*/
+    let Dcomment = ft_creatChild("div", "div_comment", "group-comment", i);
+    Div.appendChild(Dcomment);
+    /*add comment*/
+    let name = ft_creatChild("p", "name", "group-comment", i);
+    Div.appendChild(name);
+    document.getElementById("name" + i).textContent += data.name[i];
+    let year = ft_creatChild("p", "year", "group-comment", i);
+    Div.appendChild(year);
+    document.getElementById("year" + i).textContent += data.year[i];
+  }
+  scrollDivs();
 }
 
-function ft_style_Div(element)
+function ft_name_pic(path, nb)
 {
-    // element.style.width = "200px";
-    // element.style.height = "100px";
-    // element.style.backgroundColor = randomColor();
+  path = "./resources/pictures/pic-" + nb + ".JPG"
+  return path;
 }
 
 function randomColor()
@@ -63,35 +82,10 @@ function scrollDivs()
 /*creat element*/
 function ft_creatChild(type, nameId, nameClass, i)
 {
-    let div = document.createElement(type, i);
+    let div = document.createElement(type);
     div.id = nameId + i;
     div.className = nameClass;
     return div;
-}
-
-function ft_creatChaildsOfChaild(parent, data)
-{
-    // let child = document.getElementById(parent);
-    //     // let element1 = ft_creatChild("img", "Imag", "group-imags", 1);
-    //     // ft_style_Image/(element1);
-    //     // child.appendChild(element1);
-
-    //     let ul = ft_creatChild("ul", "Ul", "group-ul", 1);
-    //     child.appendChild(ul);
-    //     // ul.style.backgroundColor = "#000000";
-    //     for (let i = 0; i < 3; i++) {
-    //       let el = data[i];
-          
-    //       let listItem =  ft_creatChild("li", "Li", "group-li", i);
-    //       // listItem.textContent = el;
-    //       // listItem.style.alignContent.color = "#fefefe";
-    //       ul.appendChild(listItem);
-
-        // }
-        let element = ft_creatChild("div", "Div", "group-divs", 1);
-        // ft_style_Div(element);
-        bigBox.appendChild(element);
-        ft_creatChaildsOfChaild(element, data);
 }
 
 main();
